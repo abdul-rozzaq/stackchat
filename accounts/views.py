ba843a3764aea8a22aba0013d5fe2f5c9879d6a7
rayyandeveloper <rayyan.dev@mail.ru>
2023-04-06 14:04:04 +0500

    mmm

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import redirect, render
 from django.views.generic import View
 
+from django.contrib.auth.models import User
+
 
 class RegisterView(View):
     def get(self,request):
         return render(request, 'accounts/register.html')
     
     
-    def post(self, request):
-        return render(request, 'accounts/register.html')
+    def post(self, requets):        
+        
+        try:
+            first_name = requets.POST.get('first-name')
+            last_name = requets.POST.get('last-name')
+            username = requets.POST.get('username')
+            email = requets.POST.get('email')
+            
+            password = requets.POST.get('password')
+             
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username)    
+                        
+        except Exception as e:
+            print(f'Register error {str(e).strip()}')
+        
+        return render(requets, 'accounts/register.html')
         
         
 class LoginView(View):
@@ -16,6 +32,8 @@ class LoginView(View):
         return render(request, 'accounts/login.html')
     
     
-    def post(self, request):
-        return render(request, 'accounts/login.html')
+    def post(self, requets):
+        
+        
+        return render(requets, 'accounts/login.html')
         
